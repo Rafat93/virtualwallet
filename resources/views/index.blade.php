ba6528af943a38148f2f56d5d8854cd2d4ac7875
Eduarda Ramos <2171347@my.ipleiria.pt>
2019-12-10 17:50:45 +0000

    Adicionei views.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('master')
+
+@section('title', 'Virtual Wallet')
+
+@section('content')
+
+        <router-view></router-view>
+
+@endsection
+
+@section('pagescript')
+<script src="js/app.js"></script>
+ @stop
