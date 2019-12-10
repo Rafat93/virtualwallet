2387d1e874942fa6572e965dd4d4a664b3575458
Rafael Correia <2171295@my.ipleiria.pt>
2019-12-10 17:50:11 +0000

    Routes added

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,31 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth:api'])->group(function (){
+   //wallet
+    Route::get('wallets','Api\WalletController@index');
+    Route::get('users/{id}/wallet/my','Api\WalletController@getMyWallet');
+
+    //movements
+    Route::get('movements','Api\MovementController@index');
+
+    //user
+    Route::get('users/me', 'Api\UserController@myProfile');
+    Route::get('users/{id}', 'Api\UserController@show'); //find user
+    Route::post('users', 'Api\UserController@store');
+    Route::put('users/{id}', 'Api\UserController@update');
+    Route::delete('users/{id}', 'Api\UserController@destroy');
+
+    Route::post('logout', 'Api\LoginController@logout');
+
+});
+
+//-------------------------Wallet Routes--------------------------------------
+Route::get('wallets/number','Api\WalletController@getNumberOfWallets');
+//-------------------------user Routes--------------------------------------
+Route::get('users/emailavailable', 'Api\UserController@emailAvailable');
+//------------------------Auth Routes---------------------------------------
+Route::post('login', 'Api\LoginController@login')->name('login');
+
+//--------------------------------------------------------------------------
