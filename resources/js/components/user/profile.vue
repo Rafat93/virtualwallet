a0ffe9fea1913fc29f440fcebec7ee639d12dd0f
Eduarda Ramos <2171347@my.ipleiria.pt>
2019-12-16 20:06:32 +0000

    Created profile view but photo is missing.

--- resources/js/components/user/profile.vue
+++ resources/js/components/user/profile.vue
@@ -5,13 +5,55 @@
             <div class="content_title">
                 Profile
             </div>
+
+            <div class="card" style="margin-left: 80px; margin-right: 80px;">
+                <div class="card-header">
+                    Personal Information
+                    <button class="btn btn-sm btn-dark" style="float: right;">Edit</button>
+                </div>
+                <div class="card-body ">
+                    <div class="form-group">
+<!--
+                        <img src="/database/seeds/fotos/{{user.photo}}">
+-->
+                    </div>
+
+                    <p class="font-weight-normal">Name:</p>
+                    <p class="font-weight-light" >{{user.name}}</p>
+                    <p class="font-weight-normal">Email:</p>
+                    <p class="font-weight-light" >{{user.email}}</p>
+                    <p class="font-weight-normal">NIF:</p>
+                    <p class="font-weight-light" >{{user.nif}}</p>
+                    <p class="font-weight-normal">Type:</p>
+                    <p class="font-weight-light" >{{user.type}}</p>
+
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "profile"
+       export default {
+        data: function(){
+            return {
+                user: '',
+            }
+        },
+        methods: {
+            getUserInfo: function(){
+                //console.log(this.$store.state.token);
+
+                axios.get('api/users/me',{'headers': {'Authorization': 'Bearer '+this.$store.state.token}})
+                    .then(response=>{this.user = response.data.data; });
+
+
+            },
+        },
+
+        mounted() {
+            this.getUserInfo();
+        }
     }
 </script>
 
