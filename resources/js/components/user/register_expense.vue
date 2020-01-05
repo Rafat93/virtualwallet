f396ede96cd4454097ec8474349786a843989d77
Eduarda Ramos <2171347@my.ipleiria.pt>
2020-01-05 16:25:05 +0000

    Register_expense form is done but not working yet.

--- resources/js/components/user/register_expense.vue
+++ resources/js/components/user/register_expense.vue
@@ -1,89 +1,76 @@
 <template>
-    <div style="overflow: unset;">
+    <div>
         <navbar/>
         <div class="inside_content" >
             <div class="content_title">
-                Register a new expense
+                Registe a new expense
             </div>
+            <div class="card" style="margin-left: 200px; margin-right: 200px;">
+                <div class="card-header">
+                    Information about the expense
+                </div>
+                <div class="card-body" style="background-color: rgba(0,0,0,0.35)">
+                    <div style="margin-right: 150px; margin-left: 150px;">
+                        <div class="form-group">
+                            <label>Type of movement</label>
+                            <b-form-select v-model="typeOfMovementSelected" :options="typeOfMovement" class="mb-3">
+                            </b-form-select>
+                        </div>
 
-            <form class="container">
-                <div class="card">
-                    <div class="card-header">
-                        Information about the expense
-                    </div>
-                    <div class="card-body" style="background-color: rgba(0,0,0,0.35)">
-                        <div style="margin-right: 150px; margin-left: 150px;">
+                        <div v-if="typeOfMovementSelected == 'transfer'" class="form-group">
+                            <label>Email:</label>
+                            <b-form-input  type="email" v-model="email" placeholder="Enter the email"></b-form-input>
+                        </div>
+                        <div v-if="typeOfMovementSelected == 'payment'" class="form-group">
+                            <label>Type of payment (to external entity):</label>
+                            <b-form-select v-model="typeOfPaymentSelected" :options="typeOfPayment" class="mb-3">
+                            </b-form-select>
+                        </div>
+
+                        <div v-if="typeOfPaymentSelected == 'transfer'" class="form-group">
                             <div class="form-group">
-                                <label>Type of movement</label>
-                                <!--<b-form-select v-model="typeOfPaymentSelected" :options="typeOfPayment" class="mb-3">
-                                    &lt;!&ndash; This slot appears above the options from 'options' prop &ndash;&gt;
-                                    <template v-slot:first>
-                                        <option :value="null" disabled>&#45;&#45; Please select an option &#45;&#45;</option>
-                                    </template>
-                                </b-form-select>-->
-                                ola
-                                {{typeOfPaymentSelected}}
-                            </div>
+                                <label>IBAN</label>
+                                <b-form-input v-model="iban" placeholder="e.g. PT50 0000 0000 0000 0000 0000 0"></b-form-input>
 
-                            <div v-if="transferMovement == payment" class="form-group">
-                                <label for="typeOfMovement">Type of movement</label>
-                                <select id="typeOfPayment" name="typeOfPayment" class="form-control">
-                                    <option value="" disabled selected>----Select a type----</option>
-                                    <option value="mbPayment" >MB Payment</option>
-                                    <option value = "bank">Bank Transfer</option>
-                                </select>
-                            </div>
-
-                            <div v-if="transferMovement == transfer" class="form-group">
-                                <label>Email:</label>
-                                <input type="email" class="form-control" placeholder="user@example.com">
-                            </div>
-
-                            <div v-if="paymentMovement == bank" class="form-group">
-                                <div class="form-group">
-                                    <label for="inputIban">IBAN</label>
-                                    <input class="form-control" type="text" id="inputIban"  maxlength="25" placeholder="e.g. PT50 0000 0000 0000 0000 0000 0">
-                                </div>
-                            </div>
-
-                            <div v-if="paymentMovement == mbPayment" class="form-group">
-                                <div class="form-group">
-                                    <label for="inputEntity">Entity</label>
-                                    <input class="form-control" type="number" id="inputEntity" placeholder="e.g. 12345" maxlength="5">
-                                </div>
-                                <div class="form-group">
-                                    <label for="inputReference">Reference</label>
-                                    <input class="form-control" type="number" id="inputReference" placeholder="e.g. 123 456 789" maxlength="9">
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="inputValue">Value</label>
-                                <input class="form-control" type="number" id="inputValue" placeholder="0,01€ - 5000,00€">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="category_id">Category</label>
-                                <select class="form-control" id="category_id">
-                                    <option value="" selected disabled>----Select an option----</option>
-                                    <option  v-for="categoria in category" v-bind:value="categoria.id">{{categoria.name}}</option>
-                                </select>
-                            </div>
-
-                            <div class="form-group">
-                                <label>Description</label>
-                                <textarea class="form-control"></textarea>
-                            </div>
-                            <div class="form-group" style="text-align: center;">
-                                <input type="submit" class="btn btn-success">
-                                <button type="reset" class="btn btn-secondary">Reset</button>
-                                <button  class="btn btn-danger">Cancel</button>
                             </div>
                         </div>
 
+                        <div v-if="typeOfPaymentSelected == 'payment'" class="form-group">
+                            <div class="form-group">
+                                <label>Entity:</label>
+                                <b-form-input v-model="entity" ></b-form-input>
+                            </div>
+                            <div class="form-group">
+                                <label>Reference:</label>
+                                <b-form-input v-model="reference" ></b-form-input>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Value:</label>
+                            <b-form-input v-model="value" type="number" ></b-form-input>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Category</label>
+                            <b-form-select v-model="categorySelected"
+                            value-field="id"
+                            text-field="name"
+                            :options="categories"  class="mb-3"></b-form-select>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Description</label>
+                            <b-form-input v-model="description" max="200"></b-form-input>
+                        </div>
+                        <div class="form-group" style="text-align: center;">
+                            <b-btn variant="success" @submit>Submit</b-btn>
+                            <b-btn variant="danger" @click="cancel">Cancel</b-btn>
+                        </div>
                     </div>
+
                 </div>
-            </form>
+            </div>
         </div>
     </div>
 </template>
@@ -93,16 +80,15 @@
 
         data:function(){
             return{
-                category: {},
-                transferMovement : '',
-                transfer : "transfer",
-                payment : "payment",
-                paymentMovement : '',
-                bank : "bank",
-                mbPayment : "mbPayment",
+                categories: [],
+                categorySelected: '',
 
-                typeOfPaymentSelected: '',
-                typeOfPayment:[
+                typeOfMovementSelected: null,
+                typeOfMovement:[
+                    {
+                        text: 'Please select an option',
+                        value: 'null'
+                    },
                     {
                         text: 'Transfer to other VirtualWallet',
                         value: 'transfer'
@@ -112,15 +98,47 @@
                         value: 'payment'
                     },
                 ],
+
+                typeOfPaymentSelected: null,
+                typeOfPayment:[
+                    {
+                        text: 'Please select an option',
+                        value: 'null'
+                    },
+                    {
+                        text: 'Bank transfer',
+                        value: 'transfer'
+                    },
+                    {
+                        text: 'MB payment',
+                        value: 'payment'
+                    },
+                ],
+
+                email:'',
+                iban:'',
+                entity:'',
+                reference:'',
+                value:'',
+                description:'',
+
             }
         },
 
         methods:{
+            getCategories(){
+                axios.get('api/categories',{'headers': {'Authorization': 'Bearer '+ this.$store.state.token}})
+                    .then(response=> {this.categories = response.data.data; });
+            },
+            cancel(){
+                this.$router.push('/welcome');
+            },
+            submit(){
+
+            }
         },
         mounted() {
-
-            axios.get('api/categories',{'headers': {'Authorization': 'Bearer '+ this.$store.state.token}})
-                .then(response=> {this.category = response.data.data; console.log(response)});
+            this.getCategories();
 
         },
 
@@ -128,16 +146,4 @@
 </script>
 
 <style scoped>
-    .inside_content{
-        margin-top: 56px;
-        margin-left: 60px;
-        overflow-y: auto;
-        height: 92vh;
-    }
-    .content_title{
-        text-align: center;
-        font-size: 30px;
-        margin-bottom: 10px;
-        font-family: "Roboto", sans-serif;
-    }
 </style>
